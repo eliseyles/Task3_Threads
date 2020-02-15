9866a33aec9ab8241a5a033f152a8245f05e2a81
Alesya Khoroneko <horoneko00@gmail.com>
2020-02-15 10:29:06 +0300

    Add Summator Thread

---
+++ src/main/java/by/training/task3/thread/MatrixSummator.java
@@ -0,0 +1,27 @@
+package by.training.task3.thread;
+
+import by.training.task3.entity.Matrix;
+
+import java.text.Format;
+import java.util.Formatter;
+import java.util.concurrent.Callable;
+
+public class MatrixSummator implements Callable {
+    static final String TEMPLATE = "Thread %d sum %d";
+
+    @Override
+    public String[] call() throws Exception {
+        Matrix matrix = Matrix.getInstance();
+        String[] result = new String[matrix.getSize()];
+        int threadSum;
+        for (int i = 0; i < matrix.getSize(); i++) {
+            threadSum = 0;
+            for (int j = 0; j < matrix.getSize(); j++) {
+                threadSum += matrix.getElementValue(i, j);
+                threadSum += matrix.getElementValue(j, i);
+            }
+            result[i] = String.format(TEMPLATE, i, threadSum);
+        }
+        return result;
+    }
+}
